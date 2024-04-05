9207e2e33b7f93ee7528e22ee68c2c0b4f5612e4
FelipeMDantas <felipemdantas@terra.com.br>
2024-04-05 15:28:57 -0300

    Landing page further developed.

---
+++ ui/src/routes/homePage/homePage.jsx
@@ -0,0 +1,40 @@
+import "./homePage.scss";
+import SearchBar from "../../components/searchBar/SearchBar";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find a Real Estate and get your Dream Place</h1>
+          <p>
+            Discovering a dream home is like unveiling a hidden treasure, a
+            sanctuary that resonates with your heart and mirrors your deepest
+            desires. It's a harmonious blend of comfort and personal style, a
+            haven where every corner tells your story.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Awards Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Properties Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
